e1304c7984ff7937cc4271ac0b8cc563d2950f29
iJullaa <sj53927@zut.edu.pl>
2025-09-28 21:55:13 +0200

    FIX(deploy): Force final axios baseURL configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://sklep-meblowy-frontend.vercel.app/';
+const API_URL = 'https://sklep-meblowy-api.onrender.com';
 
 const apiClient = axios.create({
   baseURL: API_URL,
