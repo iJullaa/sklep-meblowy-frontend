fe41675a47d5468d8617385a32001d7b6dcbf256
iJullaa <sj53927@zut.edu.pl>
2025-09-28 22:17:28 +0200

    fix(api): add /api prefix to base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://sklep-meblowy-api.onrender.com';
+const API_URL = 'https://sklep-meblowy-api.onrender.com/api';
 
 const apiClient = axios.create({
   baseURL: API_URL,
